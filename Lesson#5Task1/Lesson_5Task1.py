a36a1ab5014bc02da9967fba907e5cedc166e57c
maxstar <maksimgavura78@gmail.com>
2024-12-08 22:29:47 +0200

    Добавьте файлы проекта.

---
+++ Lesson#5Task1/Lesson_5Task1.py
@@ -0,0 +1,12 @@
+﻿number = int(input())
+
+if (number % 2 == 0) and (number > 0):
+    print("polozhitelnoe 4etnoe 4islo.")
+elif (number % 2 != 0) and (number > 0):
+    print("polozhitelnoe ne4etnoe 4islo.")
+elif (number % 2 == 0) and (number < 0):
+    print("otricatelnoe 4etnoe 4islo")
+elif (number % 2 != 0) and (number < 0):
+    print ("otricatelnoe ne4etnoe 4islo")
+else:
+    print("4islo ravno nulju")
